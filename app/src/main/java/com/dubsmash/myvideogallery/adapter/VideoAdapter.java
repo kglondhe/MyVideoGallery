4e6cafeac25de9988870c1669ac1c3b1a3cc5feb
kappsy <kglondhe@gmail.com>
2017-06-01 15:31:27 -0400

    Fix thumbnail issue

--- app/src/main/java/com/dubsmash/myvideogallery/adapter/VideoAdapter.java
+++ app/src/main/java/com/dubsmash/myvideogallery/adapter/VideoAdapter.java
@@ -1,7 +1,10 @@
 package com.dubsmash.myvideogallery.adapter;
 
+import android.content.Context;
+import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.media.ThumbnailUtils;
+import android.net.Uri;
 import android.provider.MediaStore;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -19,11 +22,12 @@ import java.util.ArrayList;
  * Created by kapillondhe on 01/06/17.
  */
 
-public class VideoAdapter extends RecyclerView.Adapter<VideoViewHolder>{
+public class VideoAdapter extends RecyclerView.Adapter<VideoViewHolder> {
 
     private final ArrayList<VideoInformation> videoInformations;
     private final ActivityCallBack activityCallBack;
-    public VideoAdapter(ArrayList<VideoInformation> videoInformations, ActivityCallBack activityCallBack){
+
+    public VideoAdapter(ArrayList<VideoInformation> videoInformations, ActivityCallBack activityCallBack) {
         this.videoInformations = videoInformations;
         this.activityCallBack = activityCallBack;
 
@@ -35,7 +39,6 @@ public class VideoAdapter extends RecyclerView.Adapter<VideoViewHolder>{
         final View itemView = LayoutInflater.
                 from(parent.getContext()).
                 inflate(R.layout.video_item, parent, false);
-
 
 
         return new VideoViewHolder(itemView);
@@ -53,18 +56,32 @@ public class VideoAdapter extends RecyclerView.Adapter<VideoViewHolder>{
         });
         holder.videoDuration.setText(DateUtil.convertToHHMMSS(videoInformation.getDuration()));
         holder.videoDate.setText(DateUtil.convertToDate(videoInformation.getTime()));
-        final Bitmap bitmap  = getVideoThumbnail(videoInformation.getPath());
-        if(bitmap != null) {
+        final Bitmap bitmap = getVideoThumbnail(getPathFromURI(holder.parentView.getContext(),videoInformation.getPath()));
+        if (bitmap != null) {
             holder.videoThumbnail.setImageBitmap(bitmap);
-        }else{
+        } else {
             holder.videoThumbnail.setImageResource(R.mipmap.ic_launcher);
         }
 
     }
 
-    private Bitmap getVideoThumbnail(String path){
-        Bitmap bitmap =ThumbnailUtils.createVideoThumbnail(path, MediaStore.Video.Thumbnails.MICRO_KIND);
+    private Bitmap getVideoThumbnail(String path) {
+        Bitmap bitmap = ThumbnailUtils.createVideoThumbnail(path, MediaStore.Video.Thumbnails.MICRO_KIND);
         return bitmap;
+    }
+
+    private String getPathFromURI(Context context, String uriString) {
+        final Uri uri = Uri.parse(uriString);
+        final String[] filePathColumn = {MediaStore.Images.Media.DATA};
+        final Cursor cursor = context.getContentResolver().query(uri, filePathColumn, null, null, null);
+        String realPath = null;
+        if (cursor != null && cursor.moveToFirst()) {
+            final int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+            realPath = cursor.getString(columnIndex);
+            cursor.close();
+        }
+
+        return realPath;
     }
 
     @Override
